263d6611f0c213f1c647f63a17693edcafc6e62f
shailesh-ss-19-11 <gokhaleshail@gmail.com>
2023-11-25 09:34:14 +0530

    small cnages

--- src/components/site/Home.jsx
+++ src/components/site/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './site.scss';
+import { CiSearch } from "react-icons/ci";
 const Home = () => {
     return (
         <div className='home'>
+            <div className='d-flex border border-primary'>
+                <div><CiSearch /></div>
+                <div><input type="text" className='border-none' /></div>
+            </div>
             <div className='bg-1'>
 
             </div>
             <p className='heading'>Home </p>
+
         </div>
     );
 }
